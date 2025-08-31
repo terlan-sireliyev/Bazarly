99626c2398576001b20e3d7ab7828866e04d3157
terlan-sireliyev <sireliyev95@list.ru>
2025-08-31 22:18:49 +0300

    responsive planset size

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -3,36 +3,34 @@ import { products } from "../../utils/productList";
 import Categories from "../../features/Categories/components/Categories";
 
 const HomePage = () => {
-  //sadsad
   return (
     <main className="max-w-7xl mx-auto px-6 ">
       <Categories />
-  <div
-  className="
+      <div
+        className="
     grid 
     grid-cols-1              /* ən kiçik ekranlarda → 1 sütun */
-    sm:grid-cols-2           /* 640px və yuxarı → 2 sütun */
+    sm:grid-cols-3           /* 640px və yuxarı → 2 sütun */
     md:grid-cols-3           /* 768px və yuxarı → 3 sütun */
     xl:grid-cols-4           /* 1280px və yuxarı → 4 sütun */
     gap-4
   "
->
-  {products.map((item, index) => (
-    <ProductCard
-      key={index}
-      category={item.category}
-      title={item.name}
-      image={item.image}
-      location={`${item.city}`}
-      date={item.date}
-      time={item.time}
-      user={item.user}
-      condition={item.condition}
-      price={item.price}
-    />
-  ))}
-</div>
-
+      >
+        {products.map((item, index) => (
+          <ProductCard
+            key={index}
+            category={item.category}
+            title={item.name}
+            image={item.image}
+            location={`${item.city}`}
+            date={item.date}
+            time={item.time}
+            user={item.user}
+            condition={item.condition}
+            price={item.price}
+          />
+        ))}
+      </div>
     </main>
   );
 };
