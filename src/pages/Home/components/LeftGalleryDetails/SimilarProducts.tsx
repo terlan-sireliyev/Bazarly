85f353486b9d07c8817a28d798a2e2477bc582ba
terlan-sireliyev <sireliyev95@list.ru>
2025-09-07 23:51:57 +0300

    similar prodcuts

--- src/pages/Home/components/LeftGalleryDetails/SimilarProducts.tsx
+++ src/pages/Home/components/LeftGalleryDetails/SimilarProducts.tsx
@@ -23,7 +23,7 @@ const SimilarProducts: React.FC<SimilarProductsProps> = ({
 
   return (
     <div className="my-8">
-      <h2 className="text-2xl font-bold mb-6 text-gray-800">Oxşar Məhsullar</h2>
+      <h2 className="text-2xl font-bold mb-6 ml-2 text-gray-800">Oxşar Məhsullar</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
         {similarProducts.map((ad) => (
           <Link
